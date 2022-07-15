3bb71ae1378f3332f9bf428e799bed14ea9ca2f5
Sean <56557448+shagem@users.noreply.github.com>
2022-07-15 14:27:59 -0400

    Add files via upload

---
+++ js/pca_js.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function(){
+	$(".barImg").show();
+});
+
+$(document).ready(function(){
+	$(".homefont").fadeIn(4000);
+});
+
+$(document).ready(function(){
+	$(".fa-dna").fadeIn(4000);
+});
+
+$(document).ready(function(){
+	$(".fade_body").fadeIn(3000);
+});
+
+
